d4b2822bbf6621670781a9a9c7196221afbbd99e
Andy Humes <andrew.humes@du.edu>
2022-02-12 15:59:53 -0700

    removed modal, working on adding css

--- public/js/patient.js
+++ public/js/patient.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
     const illnessField = $('#illnessField');
     const doctorNotesField = $('#doctorNotesField');
 	const addPatientBtn = $("#addPatientBtn");
+	const addNotesBtn = $("#addNotesBtn");
+
 	addPatientBtn.on('click', async function(event) {
 		event.preventDefault();
 		await $.post('/api/patients', {
@@ -10,6 +12,24 @@ $(document).ready(function() {
             illness: illnessField.val(),
             doctorNotes: doctorNotesField.val(),
 		});
+
+		
 		window.location.reload();
 	})
+
+	addNotesBtn.on('click', async function(event) {
+		event.preventDefault();
+		await $.patch('/api/patients', {
+			patientName: patientNameField.val(),
+            illness: illnessField.val(),
+            doctorNotes: doctorNotesField.val(),
+		});
+		
+		
+		window.location.reload();
+	})
+
+	
 });
+
+
